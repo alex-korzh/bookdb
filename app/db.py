849dc7f9b63f8fc07f875aa012830474b075fb61
alex-korzh <alex@korzh.me>
2022-07-30 01:03:28 +0300

    fix for pycharm

--- app/db.py
+++ app/db.py
@@ -1,15 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeMeta
 
 from app.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
+Base: DeclarativeMeta = declarative_base()
 
 
-async def get_session() -> AsyncSession:  # type: ignore
+async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
         await session.commit()
